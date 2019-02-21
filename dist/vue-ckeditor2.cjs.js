0f2e22d2f6a0c41cbb8ecee12500f3aebb2e59c8
peter <peter@emotion.nl>
2019-02-21 16:38:39 +0100

    Update events from camelCase to kebab-case

--- dist/vue-ckeditor2.cjs.js
+++ dist/vue-ckeditor2.cjs.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 //
 //
 //
+//
 var inc = new Date().getTime();
 var script = {
   name: 'VueCkeditor',
@@ -79,9 +80,6 @@ var script = {
   mounted: function mounted() {
     this.create();
   },
-  beforeDestroy: function beforeDestroy() {
-    this.destroy();
-  },
   methods: {
     create: function create() {
       var _this = this;
@@ -113,15 +111,15 @@ var script = {
         }); // Ckeditor contentDom event
 
         this.instance.on('contentDom', function(evt) {
-          _this.$emit('contentDom', evt);
+          _this.$emit('content-dom', evt);
         }); // Ckeditor dialog definition event
 
         CKEDITOR.on('dialogDefinition', function(evt) {
-          _this.$emit('dialogDefinition', evt);
+          _this.$emit('dialog-definition', evt);
         }); // Ckeditor file upload request event
 
         this.instance.on('fileUploadRequest', function(evt) {
-          _this.$emit('fileUploadRequest', evt);
+          _this.$emit('file-upload-request', evt);
         }); // Ckditor file upload response event
 
         this.instance.on('fileUploadResponse', function(evt) {
@@ -129,12 +127,16 @@ var script = {
             _this.onChange();
           }, 0);
 
-          _this.$emit('fileUploadResponse', evt);
+          _this.$emit('file-upload-response', evt);
         }); // Listen for instanceReady event
 
         if (typeof this.instanceReadyCallback !== 'undefined') {
           this.instance.on('instanceReady', this.instanceReadyCallback);
-        }
+        } // Registering the beforeDestroyed hook right after creating the instance
+
+        this.$once('hook:beforeDestroy', function() {
+          _this.destroy();
+        });
       }
     },
     update: function update(val) {
@@ -176,160 +178,145 @@ var script = {
   }
 };
 
-/* script */
-var __vue_script__ = script;
-/* template */
-
-var __vue_render__ = function __vue_render__() {
-  var _vm = this;
-
-  var _h = _vm.$createElement;
-
-  var _c = _vm._self._c || _h;
-
-  return _c(
-    'div',
-    {
-      staticClass: 'ckeditor'
-    },
-    [
-      _c('textarea', {
-        attrs: {
-          name: _vm.name,
-          id: _vm.id,
-          types: _vm.types,
-          config: _vm.config,
-          disabled: _vm.readOnlyMode
-        },
-        domProps: {
-          value: _vm.value
-        }
-      })
-    ]
-  );
-};
-
-var __vue_staticRenderFns__ = [];
-__vue_render__._withStripped = true;
-/* style */
-
-var __vue_inject_styles__ = undefined;
-/* scoped */
-
-var __vue_scope_id__ = undefined;
-/* module identifier */
-
-var __vue_module_identifier__ = undefined;
-/* functional template */
-
-var __vue_is_functional_template__ = false;
-/* component normalizer */
-
-function __vue_normalize__(
+function normalizeComponent(
   template,
   style,
-  script$$1,
-  scope,
-  functional,
+  script,
+  scopeId,
+  isFunctionalTemplate,
   moduleIdentifier,
+  /* server only */
+  shadowMode,
   createInjector,
-  createInjectorSSR
+  createInjectorSSR,
+  createInjectorShadow
 ) {
-  var component =
-    (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {}; // For security concerns, we use only base name in production mode.
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
 
-  component.__file =
-    '/Users/dangvanthanh/Code/dev/vue/vue-ckeditor2/src/VueCkeditor.vue';
+  var options = typeof script === 'function' ? script.options : script; // render functions
 
-  if (!component.render) {
-    component.render = template.render;
-    component.staticRenderFns = template.staticRenderFns;
-    component._compiled = true;
-    if (functional) component.functional = true;
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+  if (scopeId) {
+    options._scopeId = scopeId;
   }
 
-  component._scopeId = scope;
+  var hook;
 
-  return component;
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode
+      ? function() {
+          style.call(
+            this,
+            createInjectorShadow(this.$root.$options.shadowRoot)
+          );
+        }
+      : function(context) {
+          style.call(this, createInjector(context));
+        };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
 }
+
+var normalizeComponent_1 = normalizeComponent;
+
+/* script */
+const __vue_script__ = script;
+
+/* template */
+var __vue_render__ = function() {
+  var _vm = this;
+  var _h = _vm.$createElement;
+  var _c = _vm._self._c || _h;
+  return _c('div', { staticClass: 'ckeditor' }, [
+    _c('textarea', {
+      attrs: {
+        name: _vm.name,
+        id: _vm.id,
+        types: _vm.types,
+        config: _vm.config,
+        disabled: _vm.readOnlyMode
+      },
+      domProps: { value: _vm.value }
+    })
+  ]);
+};
+var __vue_staticRenderFns__ = [];
+
+/* style */
+const __vue_inject_styles__ = undefined;
+/* scoped */
+const __vue_scope_id__ = undefined;
+/* module identifier */
+const __vue_module_identifier__ = undefined;
+/* functional template */
+const __vue_is_functional_template__ = false;
 /* style inject */
 
-function __vue_create_injector__() {
-  var head = document.head || document.getElementsByTagName('head')[0];
-  var styles =
-    __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
-  var isOldIE =
-    typeof navigator !== 'undefined' &&
-    /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-  return function addStyle(id, css) {
-    if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) return; // SSR styles are present.
-
-    var group = isOldIE ? css.media || 'default' : id;
-    var style =
-      styles[group] ||
-      (styles[group] = {
-        ids: [],
-        parts: [],
-        element: undefined
-      });
-
-    if (!style.ids.includes(id)) {
-      var code = css.source;
-      var index = style.ids.length;
-      style.ids.push(id);
-
-      if (isOldIE) {
-        style.element =
-          style.element ||
-          document.querySelector('style[data-group=' + group + ']');
-      }
-
-      if (!style.element) {
-        var el = (style.element = document.createElement('style'));
-        el.type = 'text/css';
-        if (css.media) el.setAttribute('media', css.media);
-
-        if (isOldIE) {
-          el.setAttribute('data-group', group);
-          el.setAttribute('data-next-index', '0');
-        }
-
-        head.appendChild(el);
-      }
-
-      if (isOldIE) {
-        index = parseInt(style.element.getAttribute('data-next-index'));
-        style.element.setAttribute('data-next-index', index + 1);
-      }
-
-      if (style.element.styleSheet) {
-        style.parts.push(code);
-        style.element.styleSheet.cssText = style.parts
-          .filter(Boolean)
-          .join('\n');
-      } else {
-        var textNode = document.createTextNode(code);
-        var nodes = style.element.childNodes;
-        if (nodes[index]) style.element.removeChild(nodes[index]);
-        if (nodes.length) style.element.insertBefore(textNode, nodes[index]);
-        else style.element.appendChild(textNode);
-      }
-    }
-  };
-}
 /* style inject SSR */
 
-var VueCkeditor = __vue_normalize__(
-  {
-    render: __vue_render__,
-    staticRenderFns: __vue_staticRenderFns__
-  },
+var VueCkeditor = normalizeComponent_1(
+  { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
   __vue_inject_styles__,
   __vue_script__,
   __vue_scope_id__,
   __vue_is_functional_template__,
   __vue_module_identifier__,
-  __vue_create_injector__,
+  undefined,
   undefined
 );
 
